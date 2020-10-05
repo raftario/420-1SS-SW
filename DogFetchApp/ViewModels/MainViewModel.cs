d7731b97bcba9fdf56b83925cea07de859d70d3d
Raphaël Thériault <raphael_theriault@outlook.com>
2020-10-05 10:33:26 -0400

    i18n

--- DogFetchApp/ViewModels/MainViewModel.cs
+++ DogFetchApp/ViewModels/MainViewModel.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using DogFetchApp.Commands;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Threading;
+using System.Windows;
 
 namespace DogFetchApp.ViewModels
 {
     class MainViewModel : BaseViewModel
     {
+        public DelegateCommand<string> ChangeLanguageCommand { get; set; }
 
+        public MainViewModel()
+        {
+            ChangeLanguageCommand = new DelegateCommand<string>(ChangeLanguage);
+        }
+
+        internal void ChangeLanguage(string lang)
+        {
+            if (Properties.Settings.Default.Language == lang)
+            {
+                return;
+            }
+
+            Properties.Settings.Default.Language = lang;
+            Properties.Settings.Default.Save();
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(lang);
+
+            var res = MessageBox.Show("Changing languages requires an application restart. Do you want to restart now?", "Restart Application", MessageBoxButton.YesNo);
+            if (res == MessageBoxResult.Yes)
+            {
+                Process.Start(Process.GetCurrentProcess().MainModule.FileName);
+                Application.Current.Shutdown();
+            }
+        }
     }
 }
