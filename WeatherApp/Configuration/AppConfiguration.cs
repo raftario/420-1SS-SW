6ceed4cad20c6067c6acc6b3b10e141fe37e384e
Raphaël Thériault <raphael_theriault@outlook.com>
2020-11-16 15:48:48 -0500

    Configuration

---
+++ WeatherApp/Configuration/AppConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Configuration
+{
+    public static class AppConfiguration
+    {
+        private static IConfiguration configuration;
+
+        public static string GetValue(string key)
+        {
+            if (configuration is null)
+            {
+                initConfig();
+            }
+
+            return configuration[key];
+        }
+
+        private static void initConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            builder.AddUserSecrets<MainWindow>();
+
+            configuration = builder.Build();
+        }
+    }
+}
